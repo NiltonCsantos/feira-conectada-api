e040d90505cd73afef8fb4692f9e640e79afdbcc
NiltonCsantos <niltoncesarsantos468@gmail.com>
2024-01-15 23:13:29 -0300

    Initial commit

---
+++ src/main/java/org/feiraconectada/feiraconectadaapi/FeiraConectadaApiApplication.java
@@ -0,0 +1,13 @@
+package org.feiraconectada.feiraconectadaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiraConectadaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeiraConectadaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/feiraconectada/feiraconectadaapi/FeiraConectadaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.feiraconectada.feiraconectadaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeiraConectadaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
